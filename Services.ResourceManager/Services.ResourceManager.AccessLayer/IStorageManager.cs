222263e1c3c3a595e402fde485b43e66897a8a4c
Jeevanram Kasiviswanathan <jeevan@Xome.com>
2023-02-24 22:32:40 -0600

    Revert "Using Stowage dotnet library to download,upload and delete files - Azure blob storage"
    This reverts commit 2dceba056e20dc62a798c66dcade76e89890f125.

--- Services.ResourceManager/Services.ResourceManager.AccessLayer/IStorageManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Services.ResourceManager.AccessLayer
-{
-    public interface IStorageManager
-    {
-        Task<byte[]> GetFileAsync(string blobPath);
-        Task UploadFileAsync(string filePath, string blobPath);
-        public Task DeleteFileAsync(string blobPath);
-    }
-}
